ae587a07ec727482d656b43027fdb28ca322502f
Anurag Raj <adityanv8@gmail.com>
2025-08-29 05:39:44 +0530

    1.FindIthBit 2.Set Ith Bit Value 3.Toggling Ith Bit

--- src/Bit_Manipulation/FindIthBitValue.java
+++ src/Bit_Manipulation/FindIthBitValue.java
@@ -4,18 +4,13 @@ import java.util.Scanner;
 
 public class FindIthBitValue {
     public static void Brute(int no,int i) {
-        System.out.println("The Bit is "+((no>>i)&1));
+        System.out.println("The Ith Bit is "+((no>>i)&1));
     }
 
     public static void Using_leftShift(int no,int i) {
-
-        int mask = 1 << i;
+        int mask = 1<<i;
         int ans = no & mask;
-        if (ans != 0)
-            System.out.println("The Bit is 1");
-        else
-            System.out.println("The Bit is 0");
-
+        System.out.println("The Answer is "+ans);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
--- src/Bit_Manipulation/Toogling_the_Bit.java
+++ src/Bit_Manipulation/Toogling_the_Bit.java
@@ -2,13 +2,13 @@ package Bit_Manipulation;
 
 import java.util.Scanner;
 
-public class SetIthBit {
+public class Toogling_the_Bit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int no = sc.nextInt();
         int i = sc.nextInt();
         int mask = 1<<i;
-        int ans = no | mask;
+        int ans = no^mask;
         System.out.println("The Answer is "+ans);
     }
 }
