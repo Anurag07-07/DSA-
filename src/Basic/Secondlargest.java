166728adc019fa5a636d5703eef794a433ad10e4
Anurag Raj <adityanv8@gmail.com>
2025-09-03 10:27:04 +0530

    Bit_Practice

--- src/Basic/Secondlargest.java
+++ src/Basic/Secondlargest.java
@@ -4,13 +4,12 @@ public class Secondlargest {
     public static void main(String[] args) {
         int[] arr= {5,3,4,8,9,1};
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            max = Math.max(arr[i],max);
-        }
-
         int s_max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i]!=max && arr[i]>s_max) s_max = arr[i];
+        for (int x:arr){
+            if (x>max){
+                s_max = max;
+                max = x;
+            }
         }
 
         System.out.println(s_max);
