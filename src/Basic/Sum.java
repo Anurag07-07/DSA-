af26385815aa58043ccab831479df78f810d7182
Anurag Raj <adityanv8@gmail.com>
2025-09-05 22:47:22 +0530

    Bit_Practice

---
+++ src/Basic/Sum.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class Sum {
+    static void Sum(int a ,int b) {
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        Sum(45,56);
+    }
+}
