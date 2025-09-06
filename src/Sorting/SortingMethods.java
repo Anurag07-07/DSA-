578256817d03ed460633f4b43732a66f7a34d778
Anurag Raj <adityanv8@gmail.com>
2025-09-06 23:18:17 +0530

    Sorting Methods

---
+++ src/Sorting/SortingMethods.java
@@ -0,0 +1,13 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SortingMethods {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1,5,4};
+
+//        Method 1
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
