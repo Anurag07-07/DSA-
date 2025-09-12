9174a9fb1c0fd9e12ede2ea2a09afc4fd32914d1
Anurag Raj <adityanv8@gmail.com>
2025-09-12 10:35:20 +0530

    Toogle char

---
+++ src/String/ToggleChar.java
@@ -0,0 +1,19 @@
+package String;
+
+import java.util.Arrays;
+
+public class ToggleChar {
+    public static void main(String[] args) {
+        char[] s = {'S','n','A','k','E'};
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i]>='A' && s[i]<='Z'){
+                s[i] = (char)(s[i]+32);
+            }else {
+                s[i] = (char)(s[i]-32);
+            }
+        }
+
+        System.out.println(Arrays.toString(s));
+    }
+}
