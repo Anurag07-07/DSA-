c8477f1dbf57d5a68c1a5dfd19fc9aaba83b57cb
Anurag Raj <adityanv8@gmail.com>
2025-09-15 10:15:16 +0530

    Contains Duplicate Brute

--- src/Leetcode/ContaisnDuplicate.java
+++ src/Leetcode/ContaisnDuplicate.java
@@ -1,9 +1,10 @@
 package Leetcode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /*
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
-
 
 Example 1:
 
@@ -41,7 +42,42 @@ Constraints:
 
  */
 public class ContaisnDuplicate {
-    public static void main(String[] args) {
 
+    public static boolean Brute(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int value = nums[i];
+            for (int j = 0; j < nums.length; j++) {
+                if (i!=j && nums[j]==value) return true;
+            }
+        }
+        return false;
+    }
+
+//    public static boolean Better(int[] nums) {
+//        Arrays.sort(nums);
+//        for (int i = 1; i < nums.length ; i++) {
+//            if (nums[i]==nums[i-1]) return true;
+//        }
+//        return false;
+//    }
+
+//    public static boolean Optimal(int[] nums ) {
+//        HashSet<Integer> s = new HashSet<>();
+//        for (Integer x: nums){
+//            if (s.contains(x)){
+//                return true;
+//            }
+//            s.add(x);
+//        }
+//        return false;
+//    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,1};
+        if (Brute(arr)){
+            System.out.println("Contains Duplicate");
+        }else {
+            System.out.println("Not Contains Duplicate");
+        }
     }
 }
