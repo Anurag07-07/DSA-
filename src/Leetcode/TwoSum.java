30623783ca3591eac1d608dce16e8ff13d26d1b6
Anurag Raj <adityanv8@gmail.com>
2025-09-16 10:14:46 +0530

    TwoSum Brute

--- src/Leetcode/TwoSum.java
+++ src/Leetcode/TwoSum.java
@@ -35,4 +35,39 @@ Follow-up: Can you come up with an algorithm that is less than O(n2) time comple
  */
 
 public class TwoSum {
+
+    public static int Binary_Search(int[] arr,int target) {
+        int start = 0,end = arr.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+            if (arr[mid]==target){
+                return mid;
+            } else if (arr[mid]>target) {
+                end = mid-1;
+            }else {
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    
+    public static void Brute(int[] arr,int target ) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i]+arr[j] == target){
+                    System.out.println(i+" "+j);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        Brute(arr,9);
+    }
 }
