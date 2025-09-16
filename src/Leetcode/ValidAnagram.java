5adc42fa6aac1ec0896a7c96909cd5f0734eff58
Anurag Raj <adityanv8@gmail.com>
2025-09-16 08:53:05 +0530

    ValidAnagram

--- src/Leetcode/ValidAnagram.java
+++ src/Leetcode/ValidAnagram.java
@@ -1,9 +1,9 @@
 package Leetcode;
 
+import java.util.Arrays;
+
 /*
 Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-
 
 Example 1:
 
@@ -28,6 +28,21 @@ s and t consist of lowercase English letters.
  */
 public class ValidAnagram {
     public static void main(String[] args) {
+    String s = "anagram";
+    String t = "nagaram";
 
+
+    char[] s1 = s.toCharArray();
+    char[] t1 = t.toCharArray();
+
+    //Sort The Both Array
+    Arrays.sort(s1);
+    Arrays.sort(t1);
+
+        for (int i = 0; i < s1.length; i++) {
+            if (s1[i]!=t1[i]){
+                break;
+            }
+        }
     }
 }
