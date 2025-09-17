43c780672b2037fe20b0ce056cd420c64525c6ee
Anurag Raj <adityanv8@gmail.com>
2025-09-17 10:54:09 +0530

    Remove Element Better

--- src/Leetcode/RemoveElement.java
+++ src/Leetcode/RemoveElement.java
@@ -21,15 +21,15 @@ public class RemoveElement {
         System.out.println(Arrays.toString(arr));
     }
 
-//    public static void Better(int[] arr,int val) {
-//        int k = 0;
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i]!=val){
-//                arr[k++] = arr[i];
-//            }
-//        }
-//        System.out.println(Arrays.toString(arr));
-//    }
+    public static void Better(int[] arr,int val) {
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]!=val){
+                arr[k++] = arr[i];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
     public static void main(String[] args) {
         int[] arr = {0,1,2,2,3,0,4,2};
         Brute(arr,2);
